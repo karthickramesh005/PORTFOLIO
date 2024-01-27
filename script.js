25e4c4657ee394ad457195f2c25d4469f28a3868
R Karthick <145181728+karthickramesh005@users.noreply.github.com>
2024-01-27 17:34:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ let sections = document.querySelectorAll('section');
 window.onscroll = () =>{
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop - 120;
         let height = sec.offsetHeight;
         if (top >= offset && top < offset + height){
             sec.classList.add('show-animate');
