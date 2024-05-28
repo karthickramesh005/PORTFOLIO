f4eec5231a20bc7ba366848e19520a3a088742a5
R Karthick <145181728+karthickramesh005@users.noreply.github.com>
2024-05-28 20:35:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Software Engineer.', 'Front-end Developer.', 'Web Developer.'], 'text',['#FFFFFF',' #FFFFFF','#FFFFFF']);
+consoleText(['Software Engineer.', 'Web Developer.'], 'text',['#FFFFFF','#FFFFFF']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
